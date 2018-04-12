8e104a8f67796b0e35c8a80728a035598dc6228f
Mauricio Collazos <ma0@contraslash.com>
2018-04-11 20:22:48 -0500

    totally akward but we have a minimal gentella page at /

--- abi_back/urls.py
+++ abi_back/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+
+#NoQA
+from django.views.generic import TemplateView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name="base.html"))
 ]
